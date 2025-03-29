12c5e682e0baddc0d7105d3c0bd58e5d2e381cb0
Ruan Castro <ruancastro15@gmail.com>
2025-03-29 20:10:20 -0300

    adding cascade delete

--- create_database.py
+++ create_database.py
@@ -35,6 +35,9 @@ class Store(Base):
     __tablename__ = "stores"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
+    
+    addresses = relationship("StoreAddress", back_populates="store", cascade="all, delete-orphan", passive_deletes=True)
+    phones = relationship("PhoneStore", back_populates="store", cascade="all, delete-orphan", passive_deletes=True)
 
 class Address(Base):
     __tablename__ = "addresses"
@@ -50,8 +53,10 @@ class Address(Base):
 
 class StoreAddress(Base):
     __tablename__ = "store_address"
-    store_id = Column(Integer, ForeignKey("stores.id"), primary_key=True)
+    store_id = Column(Integer, ForeignKey("stores.id", ondelete="CASCADE"), primary_key=True)
     address_id = Column(Integer, ForeignKey("addresses.id"), primary_key=True)
+    
+    store = relationship("Store", back_populates="addresses")
 
 class Customer(Base):
     __tablename__ = "customers"
@@ -59,11 +64,16 @@ class Customer(Base):
     full_name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    
+    addresses = relationship("CustomerAddress", back_populates="customer", cascade="all, delete-orphan", passive_deletes=True)
+    phones = relationship("PhoneCustomer", back_populates="customer", cascade="all, delete-orphan", passive_deletes=True)
 
 class CustomerAddress(Base):
     __tablename__ = "customer_addresses"
-    customer_id = Column(Integer, ForeignKey("customers.customer_id"), primary_key=True)
+    customer_id = Column(Integer, ForeignKey("customers.customer_id", ondelete="CASCADE"), primary_key=True)
     address_id = Column(Integer, ForeignKey("addresses.id"), primary_key=True)
+    
+    customer = relationship("Customer", back_populates="addresses")
 
 class Phone(Base):
     __tablename__ = "phones"
@@ -73,13 +83,17 @@ class Phone(Base):
 
 class PhoneCustomer(Base):
     __tablename__ = "phones_customers"
-    phone_id = Column(Integer, ForeignKey("phones.id"), primary_key=True)
-    customer_id = Column(Integer, ForeignKey("customers.customer_id"), primary_key=True)
+    phone_id = Column(Integer, ForeignKey("phones.id", ondelete="CASCADE"), primary_key=True)
+    customer_id = Column(Integer, ForeignKey("customers.customer_id", ondelete="CASCADE"), primary_key=True)
+    
+    customer = relationship("Customer", back_populates="phones")
 
 class PhoneStore(Base):
     __tablename__ = "phones_store"
-    phone_id = Column(Integer, ForeignKey("phones.id"), primary_key=True)
-    store_id = Column(Integer, ForeignKey("stores.id"), primary_key=True)
+    phone_id = Column(Integer, ForeignKey("phones.id", ondelete="CASCADE"), primary_key=True)
+    store_id = Column(Integer, ForeignKey("stores.id", ondelete="CASCADE"), primary_key=True)
+    
+    store = relationship("Store", back_populates="phones")
 
 class Purchase(Base):
     __tablename__ = "purchase"
