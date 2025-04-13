8b9e251e5b7585fdedec7776562accf0ef58f53a
Ruan Castro <ruancastro15@gmail.com>
2025-04-13 15:14:35 -0300

    draft transform

--- dags/etl_from_oltp_to_olap.py
+++ dags/etl_from_oltp_to_olap.py
@@ -82,6 +82,80 @@ class ETL():
         "phones_stores": df_phones_stores
     }
 
+    def transform(self, extracted_data: dict):
+        """Transforms the extracted OLTP data into a format suitable for the OLAP database.
+
+        This method takes the extracted data from the OLTP database, which is provided as a dictionary
+        of pandas DataFrames, and performs the necessary transformations to prepare the data for loading
+        into the OLAP database. The transformations include joining tables, aggregating data, and mapping
+        the data to the OLAP schema (dimensions and facts).
+
+        Args:
+            extracted_data (dict): A dictionary containing the extracted DataFrames from the OLTP database.
+                The dictionary keys correspond to table names, and the values are pandas DataFrames with the
+                data from those tables. Expected keys are:
+                - "customers_addresses"
+                - "phones_customers"
+                - "customers"
+                - "purchases_status"
+                - "purchases"
+                - "addresses"
+                - "phones"
+                - "items"
+                - "sizes"
+                - "stores"
+                - "prices"
+                - "categories"
+                - "inventory"
+                - "stores_addresses"
+                - "phones_stores"
+
+        Returns:
+            dict: A dictionary mapping OLAP table names to their corresponding transformed pandas DataFrames.
+                The keys are strings representing OLAP table names (e.g., "dim_customers", "fact_sales"),
+                and the values are pandas DataFrames containing the transformed data ready for loading.
+
+        Raises:
+            KeyError: If an expected key is missing from the extracted_data dictionary.
+            ValueError: If the data in the DataFrames does not meet the expected format or constraints.
+        """
+        # Carregar os DataFrames do dicionário extracted_data em variáveis locais
+        df_customers_addresses = extracted_data["customers_addresses"]
+        df_phones_customers = extracted_data["phones_customers"]
+        df_customers = extracted_data["customers"]
+        df_purchases_status = extracted_data["purchases_status"]
+        df_purchases = extracted_data["purchases"]
+        df_addresses = extracted_data["addresses"]
+        df_phones = extracted_data["phones"]
+        df_items = extracted_data["items"]
+        df_sizes = extracted_data["sizes"]
+        df_stores = extracted_data["stores"]
+        df_prices = extracted_data["prices"]
+        df_categories = extracted_data["categories"]
+        df_inventory = extracted_data["inventory"]
+        df_stores_addresses = extracted_data["stores_addresses"]
+        df_phones_stores = extracted_data["phones_stores"]
+
+        #Transform 
+        df_dim_customers = df_customers
+
+        df_dim_items = df_items
+        df_dim_items.rename(columns={'id': 'item_id'}, inplace = True)
+        df_dim_items = pd.merge(df_dim_items,df_categories,left_on = 'category_id', right_on = 'id', how = 'left')
+        df_dim_items.rename(columns={'name_x': 'name', 'name_y': 'category_name'}, inplace = True)
+        df_dim_items.drop(columns = {'category_id','id'}, inplace = True)
+
+        transformed_data = {
+            "dim_customers": None,
+            "dim_items": None,
+            "dim_sizes": None,
+            "dim_stores": None,
+            "dim_time": None,
+            "fact_sales": None,
+            "fact_inventory": None
+        }
+
+        return transformed_data
 
 if __name__ == "__main__":
     OLTP_URL = "postgresql+psycopg2://oltp:ecommerce123@localhost:5433/ecommerce_oltp"
@@ -91,4 +165,5 @@ if __name__ == "__main__":
 
     etl = ETL(oltp_url=OLTP_URL, olap_url=OLAP_URL)
     extracted_data = etl.extract()
+    transformed_data = etl.transform(extracted_data)
     print("hey")
